d00f6b0c5b5cefb43ad06480b87dceaa9c97efe9
GABRIEL JOSÉ SANTOS DA SILVA <25132745@dsn127m1247443.cfp127aluno.local>
2026-02-24 14:58:37 -0300

    Logica feita :)

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+function adicionarProduto() {
+    const produto = document.getElementById('produto');
+    const lista = document.getElementById('lista');
+    const span = document.createElement('span');
+
+    span.textContent = produto.value;
+    span.className = 'bg-blue-200 px-8 py-2 rounded-2xl flex justify-center';
+    lista.appendChild(span);
+}
+
