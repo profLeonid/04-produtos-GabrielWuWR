c6ee9451c27299a111ce919220941aa53eaac908
GABRIEL JOSÉ SANTOS DA SILVA <25132745@dsn127m1247443.cfp127aluno.local>
2026-02-24 16:40:21 -0300

    Nova interface, tratamento de campos vazios e tabela feitos

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 'use strict';
 
-function adicionarProduto() {
+// function adicionarProduto() {
+//     const produto = document.getElementById('produto');
+//     const lista = document.getElementById('lista');
+//     const span = document.createElement('span');
+
+//     if (produto.value != '') {
+//         span.textContent = produto.value;
+//         span.className = 'bg-blue-200 px-8 py-2 rounded-2xl flex justify-center';
+//         lista.appendChild(span);
+//         produto.value = "";
+//     } else {
+//         alert('ops digite algo no campo');
+//     }
+// }
+
+//caixas de texto
+
+function cadastrarProduto() {
+    const codigo = document.getElementById('codigo');
     const produto = document.getElementById('produto');
-    const lista = document.getElementById('lista');
-    const span = document.createElement('span');
+    const quantidade = document.getElementById('quantidade');
+    const tabela = document.getElementById('tabela');
 
-    span.textContent = produto.value;
-    span.className = 'bg-blue-200 px-8 py-2 rounded-2xl flex justify-center';
-    lista.appendChild(span);
+    if (codigo.value != '' && produto.value != '' && quantidade.value != '') {
+        const linha = document.createElement('tr');
+        linha.className = 'hover:bg-slate-700 transition duration-200 hover:cursor-pointer';
+
+        const itemCodigo = document.createElement('td');
+        itemCodigo.className = 'px-6 py-4';
+        itemCodigo.textContent = codigo.value;
+
+        const itemProduto = document.createElement('td');
+        itemProduto.className = 'px-6 py-4';
+        itemProduto.textContent = produto.value;
+
+        const itemQuantidade = document.createElement('td');
+        itemQuantidade.className = 'px-6 py-4';
+        itemQuantidade.textContent = quantidade.value;
+
+        linha.appendChild(itemCodigo);
+        linha.appendChild(itemProduto);
+        linha.appendChild(itemQuantidade);
+        tabela.appendChild(linha);
+    }
 }
-
